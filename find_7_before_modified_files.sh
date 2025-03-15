d2a1fa4a62a849dce28877f159dbe73d137504ef
NagleAniket <nagleaniket@gmail.com>
2025-03-15 15:11:22 +0000

    Scripts Files Moved to Linux Directory

--- find_7_before_modified_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Directory to search (default is current directory)
-DIR=${1:-.}
-
-echo "Finding files modified in the last 7 days in directory: $DIR"
-
-# Find files modified in the last 7 days
-find "$DIR" -type f -mtime -7
